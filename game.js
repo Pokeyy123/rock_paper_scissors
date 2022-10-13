2c8936853154c561574dcbb96d9bce0744b94e8d
Pokeyy123 <attilafodor603@gmail.com>
2022-10-13 16:50:17 +0300

    Made playRound function

--- game.js
+++ game.js
@@ -3,4 +3,16 @@ function getComputerChoice() {
     return elements[Math.floor(Math.random()*elements.length)]
 }
 
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return ("You lose! Paper beats rock!");
+    }
+}
 
+function game() {
+    for (let i = 0, i )
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
