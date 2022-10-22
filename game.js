6b91441d5d418cb1703f639f613504e931422f5e
Pokeyy123 <attilafodor603@gmail.com>
2022-10-22 19:33:38 +0300

    Finish game function

--- game.js
+++ game.js
@@ -5,14 +5,30 @@ function getComputerChoice() {
 
 function playRound(playerSelection,computerSelection) {
     if (playerSelection == "rock" && computerSelection == "paper") {
-        return ("You lose! Paper beats rock!");
+        return ("You lose! "+ computerSelection +" beats "+ playerSelection +"!");
+    }else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return ("You lose! "+ computerSelection +" beats "+ playerSelection +"!");
+    }else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return ("You lose! "+ computerSelection +" beats "+ playerSelection +"!");
+    }else if (playerSelection == "paper" && computerSelection == "rock") {
+        return ("You win! "+ playerSelection +" beats "+ computerSelection +"!");
+    }else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return ("You lose! "+ playerSelection +" beats "+ computerSelection +"!");
+    }else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return ("You lose! "+ playerSelection +" beats "+ computerSelection +"!");
+    }else if (playerSelection == computerSelection) {
+        return("It's a tie!")
     }
 }
 
 function game() {
-    for (let i = 0, i )
+    for (let i = 0; i < 5 ;i++ ) {
+        playRound();
+        console.log("player: "+playerSelection+" ,computer: "+computerSelection);
+    }
 }
 
-const playerSelection = "rock";
+const playerSelection = window.prompt();
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
+console.log(playRound(playerSelection,computerSelection))
+game();
